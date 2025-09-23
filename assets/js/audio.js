b75d09ebd0ef088a3fad23bbf10250367a577c58
MDNSenevi <nilu.senevi@mds.ac.nz>
2025-09-23 22:22:23 +1200

    Removed duplicate files

--- assets/js/audio.js
+++
@@ -1,7 +0,0 @@
-var soundButton = document.querySelector("#sound1Btn");
-var sound1 = document.querySelector("#sound-1");
-
-soundButton.addEventListener("click", () => {
-     sound1.currentTime = 0;
-     sound1.play();
-});
